d15bc16225948eb00715588d6dbf1edf6cbf31e4
divyesh radadiya <divyeshradadiya21@gmail.com>
2025-01-22 13:47:23 +0530

    fix : axios

--- src/config/axios-instance.ts
+++ src/config/axios-instance.ts
@@ -39,13 +39,13 @@ axiosInstance.interceptors.response.use(
   }
 );
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
 export const fetcher = (url :string) => axiosInstance.get(url).then((res) => res.data);
 
